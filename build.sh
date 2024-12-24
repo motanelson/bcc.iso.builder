758d4651c4e50052aa8fd0c7ab7e2b472e4d87c0
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2024-12-24 12:45:09 +0000

    Delete build.sh

--- build.sh
+++
@@ -1,6 +0,0 @@
-nasm mysys.s -o mysys.o
-bcc -c -Md libdos.c -o libdos.a
-bcc -x -i -L -Md mysys.c -o mysys.com
-cat mysys.o mysys.com > mysys.bin
-python3 linux.py
-qemu-system-x86_64 -cdrom cd1.iso
